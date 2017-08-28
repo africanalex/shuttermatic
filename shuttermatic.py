8742e7ea2103c0dff56a530022cb4cf4a03a1d6f
Joe <jmunapo@outlook.com>
2017-08-28 10:34:10 +0200

    first commit of count

--- shuttermatic.py
+++ shuttermatic.py
@@ -7,6 +7,36 @@ import time, os, subprocess
 win = Tk()
 
 myFont = tkFont.Font(family = "Helvetica", size = 36, weight = "bold")
+def photo_count():
+    import os
+    if(os.path.isfile('./f_count.txt')):
+        m = open('f_count.txt', 'r')
+        n = int(m.read())
+        if n <= 35:
+            m.close()
+            m = open('f_count.txt', 'w')
+            s = str(n + 1)
+            m.write(s)
+            m.close()
+            print('Photo number '+s)
+            button1Clicked()
+        else:
+            print('Please Check Catredge')
+            label["text"] = "Please Check Catredge"
+            m.close()
+            #Create a buttlon here that says Checked or Cancel
+            #if cancel it will terminate the process
+
+    else:
+        new = open('f_count.txt', 'w')
+        new.write('0')
+        new.close()
+        photo_count()
+
+def checked():
+    new = open('f_count.txt', 'w')
+    new.write('0')
+    new.close()
 
 def buttonClicked():
     print("button was clicked")
@@ -78,9 +108,9 @@ win["bg"] = "yellow"
 exitButton = Button(win, text = "Exit", font = myFont, command = exitProgram)
 exitButton.pack(side = BOTTOM)
 img4 = PhotoImage(file="4button.gif")
-startButton = Button(win, image = img4, font = myFont, command = buttonClicked)
+startButton = Button(win, image = img4, font = myFont, command = photo_count)
 img1 = PhotoImage(file="1button.gif")
-start1Button = Button(win, image=img1, command = button1Clicked)
+start1Button = Button(win, image=img1, command = photo_count)
 
 startButton.pack(side = LEFT,padx=20)
 start1Button.pack(side = RIGHT, padx=20)
