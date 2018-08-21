3d451f7c8aa914bf3811a36bf6d94af1d8c9736d
Alex Baxter <africanalex@outlook.com>
2018-08-21 13:39:46 +0100

    update

--- boothcamera.sh
+++ boothcamera.sh
@@ -4,8 +4,8 @@ DATE=$(date +"%Y-%m%d_%H%M%S")
 echo "selected camera is $value"
 if [ "$value" == "1" ]; then
 echo "Using colour"
-raspistill -vf -o /home/booth/photobooth_images/$DATE.jpg -q 100
+raspistill -hf -o /home/booth/photobooth_images/$DATE.jpg -q 100
 else
 echo "using bw"
-raspistill -vf --colfx 128:128 -o /home/booth/photobooth_images/$DATE.jpg -q 100
+raspistill -hf --colfx 128:128 -o /home/booth/photobooth_images/$DATE.jpg -q 100
 fi
